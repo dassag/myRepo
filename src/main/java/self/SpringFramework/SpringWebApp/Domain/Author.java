82a5a58723eb52abe57a783b4d4c2c7e2159788b
dassag <dassagar.official@gmail.com>
2020-04-06 20:45:10 +0530

    JPA ManytoMany

--- src/main/java/self/SpringFramework/SpringWebApp/Domain/Author.java
+++ src/main/java/self/SpringFramework/SpringWebApp/Domain/Author.java
@@ -2,11 +2,36 @@ package self.SpringFramework.SpringWebApp.Domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+
+@Data
+@Entity
 public class Author {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	
 	private String Fname;
 	private String Lname;
 	
+	@ManyToMany(mappedBy = "authors")
 	private Set<Book> books;
+
+	public Author(String fname, String lname, Set<Book> books) {
+		super();
+		Fname = fname;
+		Lname = lname;
+		this.books = books;
+	}
+	
+	public Author() {}
 
 }
--- src/main/java/self/SpringFramework/SpringWebApp/Domain/Book.java
+++ src/main/java/self/SpringFramework/SpringWebApp/Domain/Book.java
@@ -1,21 +1,42 @@
 package self.SpringFramework.SpringWebApp.Domain;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
 import lombok.Data;
 
 
 
 @Data
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
 	
 	private String title;
 	private String sbn;
 	
+	@ManyToMany
+	@JoinTable(name="author_book", joinColumns=@JoinColumn(name="books_id"),
+	inverseJoinColumns=@JoinColumn(name="author_id"))
+	private Set<Author> authors;
+	
 	public Book(){}
 
-	public Book(String title, String sbn) {
+	public Book(String title, String sbn, Set<Author> authors) {
 		super();
 		this.title = title;
 		this.sbn = sbn;
+		this.authors=authors;
 	}
 	
 	
